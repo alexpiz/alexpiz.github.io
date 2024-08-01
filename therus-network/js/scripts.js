39cf0e49662ec18708e8dd123f12fa098d776a98
Alexis Pizarro <alexis.pizarro@grupopromerica.com>
2024-08-01 16:03:22 -0600

    Countdown - controles carousel

--- therus-network/js/scripts.js
+++ therus-network/js/scripts.js
@@ -41,3 +41,33 @@ window.addEventListener('scroll', () => {
         navEl.classList.remove('navbar-scrolled');
     }
 })
+
+
+
+// Set the date we're counting down to (September 15, 2024)
+        var countDownDate = new Date("Sep 15, 2024 00:00:00").getTime();
+
+        // Update the countdown every 1 second
+        var x = setInterval(function() {
+            // Get today's date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now and the countdown date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Output the result in an element with id="countdown"
+            document.getElementById("countdown").innerHTML = "<div class='countdown-sub'> <p class='countdown-title'>" + days+ "</p>" + "<p>Días</p></div>" + "<div class='countdown-sub'> <p class='countdown-title'>" + hours + "</p>" + "<p>Horas</p></div>" +
+            "<div class='countdown-sub'> <p class='countdown-title'>" + minutes + "<p>Minutos</p></div>" + "<div class='countdown-sub'> <p class='countdown-title'>" + seconds + "<p>Segundos</p></div>";
+
+            // If the countdown is over, write some text
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("countdown").innerHTML = "EXPIRED";
+            }
+        }, 1000);
